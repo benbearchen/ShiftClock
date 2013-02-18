73a7acfac35d5d35985f9e1d2656e69e3e325812
BenBear <benbearchen@gmail.com>
2013-02-19 00:51:11 +0800

    DutyListActivity show duties in 09:30 style

--- ShiftClock/src/org/bxmy/shiftclock/DutyListActivity.java
+++ ShiftClock/src/org/bxmy/shiftclock/DutyListActivity.java
@@ -1,8 +1,12 @@
 package org.bxmy.shiftclock;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
+
+import org.bxmy.shiftclock.shiftduty.Duty;
+import org.bxmy.shiftclock.shiftduty.ShiftDuty;
 
 import android.app.Activity;
 import android.os.Bundle;
@@ -27,10 +31,22 @@ public class DutyListActivity extends Activity {
     private ArrayList<HashMap<String, Object>> addTestDuties() {
         ArrayList<HashMap<String, Object>> items = new ArrayList<HashMap<String, Object>>();
         HashMap<String, Object> map = new HashMap<String, Object>();
-        map.put("1", "白班");
-        map.put("2", new Date(0, 0, 0, 9, 30, 0));
-        map.put("3", new Date(0, 0, 0, 18, 30, 0));
-        items.add(map);
+        Duty[] duties = ShiftDuty.getInstance().getDuties();
+        for (Duty duty : duties) {
+            map = new HashMap<String, Object>();
+            map.put("1", duty.getName());
+            int start = duty.getStartSecondsInDay();
+            map.put("2", formatSecondsInDay(start));
+            int end = duty.getStartSecondsInDay() + duty.getDurationSeconds();
+            map.put("3", formatSecondsInDay(end));
+            items.add(map);
+        }
+
         return items;
+    }
+
+    private String formatSecondsInDay(int seconds) {
+        SimpleDateFormat sdf = new SimpleDateFormat("HH:mm");
+        return sdf.format(new Date(seconds * 1000L));
     }
 }
--- ShiftClock/src/org/bxmy/shiftclock/shiftduty/ShiftDuty.java
+++ ShiftClock/src/org/bxmy/shiftclock/shiftduty/ShiftDuty.java
@@ -4,10 +4,20 @@ import java.util.ArrayList;
 
 public class ShiftDuty {
 
+    private static ShiftDuty sShiftDuty;
+
     private ArrayList<Duty> mDuties = new ArrayList<Duty>();
 
-    public ShiftDuty() {
-        addTestDuties();
+    public static synchronized ShiftDuty getInstance() {
+        if (sShiftDuty == null) {
+            sShiftDuty = new ShiftDuty();
+            sShiftDuty.load();
+        }
+
+        return sShiftDuty;
+    }
+
+    private ShiftDuty() {
     }
 
     public String[] getDutyNames() {
@@ -19,9 +29,19 @@ public class ShiftDuty {
         return (String[]) names.toArray();
     }
 
+    public Duty[] getDuties() {
+        return (Duty[]) mDuties.toArray(new Duty[0]);
+    }
+
+    private void load() {
+        addTestDuties();
+    }
+
     private void addTestDuties() {
         Duty duty = new Duty();
         duty.setName("白班");
+        duty.setStartSecondsInDay(3600 * 9 + 1800);
+        duty.setDurationSeconds(3600 * 9);
         mDuties.add(duty);
     }
 }
