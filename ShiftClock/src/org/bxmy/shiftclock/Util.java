e1cc8126c28cf4b220dfd6a1d0cb300bb1793553
BenBear <benbearchen@gmail.com>
2013-09-17 13:57:14 +0800

    Modify name of Sunday

--- ShiftClock/src/org/bxmy/shiftclock/Util.java
+++ ShiftClock/src/org/bxmy/shiftclock/Util.java
@@ -96,7 +96,7 @@ public final class Util {
 
     public static String formatWeek(long dayInSeconds) {
         Date date = secondsToDate(dayInSeconds);
-        String[] weeks = { "星期天", "星期一", "星期二", "星期三", "星期四", "星期五", "星期六" };
+        String[] weeks = { "星期日", "星期一", "星期二", "星期三", "星期四", "星期五", "星期六" };
         return weeks[date.getDay() % 7];
     }
 
